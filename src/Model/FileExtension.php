4bace7af9cc0a4573ca5f6e3b251e5b7caddfdd7
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2025-01-16 15:48:29 +1300

    FIX: adding file extension

---
+++ src/Model/FileExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Sunnysideup\ShareThisSimple\Model;
+
+use SilverStripe\Assets\File;
+use SilverStripe\Core\Extension;
+use Sunnysideup\ShareThisSimple\Api\ShareThisSimpleProvider;
+
+/**
+ * Class \Sunnysideup\ShareThisSimple\Model\ShareThisSimpleExtension
+ *
+ * @property File|FileExtension $owner
+ */
+class FileExtension extends Extension
+{
+
+    public function getPinterestLink(?string $imageMethod = '', ?bool $useImageTitle = false): string
+    {
+        $owner = $this->getOwner();
+
+        return 'https://pinterest.com/pin/create/button/?url=' . ($owner->AbsoluteLink()) . '&amp;'
+            . 'description=' . rawurlencode($owner->Title) . '&amp;'
+            . 'media=' . rawurlencode($owner->AbsoluteLink());
+    }
+}
